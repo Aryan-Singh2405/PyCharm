0387c6b055528045cf5e958352873c62d3627d93
Aryan <aryansinghofficial24@gmail.com>
2026-05-15 10:39:08 +0530

    testing

--- class 1.py
+++ class 1.py
@@ -1,10 +1,39 @@
-print("this is day one of learning the python")
-s = "aryan"
-print(s)
+import random
 
-s = "dude"
-age = int(21)
-print(s, age, type(age))
+x, y, z = [random.randint(1, 100) for _ in range(3)]
 
-name, age2= input("Enter your name: ").split()
-print(name, age2)
+print("Original:", x, y, z)
+
+ans = []
+
+if x >= y and x >= z:
+    ans.append(x)
+
+    if y >= z:
+        ans.append(y)
+        ans.append(z)
+    else:
+        ans.append(z)
+        ans.append(y)
+
+elif y >= x and y >= z:
+    ans.append(y)
+
+    if x >= z:
+        ans.append(x)
+        ans.append(z)
+    else:
+        ans.append(z)
+        ans.append(x)
+
+else:
+    ans.append(z)
+
+    if x >= y:
+        ans.append(x)
+        ans.append(y)
+    else:
+        ans.append(y)
+        ans.append(x)
+
+print("Descending:", ans)
