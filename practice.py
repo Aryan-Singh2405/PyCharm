9ee724fd2b59e0f8339c78b3fabd2a2e3a44da32
Aryan <aryansinghofficial24@gmail.com>
2026-05-25 23:59:28 +0530

    testing

--- practice.py
+++ practice.py
@@ -1,18 +1,18 @@
 # QUESTION 1
-"""def product(a, b):
-    return a * b
-
-
-def sum(a, b):
-    return a + b
-
-
-a = int(input("Type the value of A: "))
-b = int(input("Type the value of B: "))
-if a * b <= 1000:
-    print("Product of A & B : ",product(a, b))
-else:
-    print("sum of A & B : ",sum(a, b))"""
+# def product(a, b):
+#     return a * b
+#
+#
+# def sum(a, b):
+#     return a + b
+#
+#
+# a = int(input("Type the value of A: "))
+# b = int(input("Type the value of B: "))
+# if a * b <= 1000:
+#     print("Product of A & B : ",product(a, b))
+# else:
+#     print("sum of A & B : ",sum(a, b))
 
 # QUESTION 2:
 # print("Printing current and previous number sum in a range(10)")
@@ -67,5 +67,38 @@ else:
 # list_one.pop(1)
 # list_one.insert(4,"fig")
 # print(list_one)
+
+# question 8:
+# name = str("Python")
+# print(f"orignal text: {name}")
+# for i in range(len(name)):
+#     print(i,name[i] )
+# print("reverse order of text:",name[::-1])
+
+# question 9:
+# sentence = str("Learning python is fun")
+# vowels = "aeiou"
+# count = 0
+# for char in sentence.lower():
+#     if char in vowels:
+#         count += 1
+#     else:
+#         continue
+# print(f"Number of vowels: {count}")
+
+# question 10:
+# nums = [45, 2, 89, 12, 7]
+# nums.sort(reverse = False)
+# print(f"the sorted order : {nums}")
+# print(f"Smallest number:{nums[0]}")
+# print(f"largest number:{nums[-1]}")
+
+# question 11
+# data = [1, 2, 2, 3, 4, 4, 4, 5]
+# data.sort()
+# print(f"the sorted order is : {data}")
+# unique_data = list(set(data))
+# print(f"the unique data is : {unique_data}")
+
 
 
