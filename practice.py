7768fe6970a8a6508c591f31becb7eb1181d3baa
Aryan <aryansinghofficial24@gmail.com>
2026-05-25 22:58:04 +0530

    testing

--- practice.py
+++ practice.py
@@ -15,5 +15,53 @@ else:
     print("sum of A & B : ",sum(a, b))"""
 
 # QUESTION 2:
-for i in range(0, 10):
+# print("Printing current and previous number sum in a range(10)")
+#
+# def operation():
+#     for value in range(0, 10):
+#         previous_value = value - 1
+#         current_value = value
+#         if previous_value == -1:
+#             previous_value = 0
+#         else:
+#             result = current_value + previous_value
+#             print(f"current value : {current_value}, previous value : {previous_value}, result : {result}")
+# operation()
+
+
+# question 3:
+
+# random_text = "pynative"
+# print("original text:",random_text)
+# print("printing only even index chars")
+# for i in range(len(random_text)):
+#     if i % 2 ==0:
+#         print(random_text[i])
+
+
+# Question 4:
+
+# text = str("pynative")
+# print(text)
+# n = int(input("type the index:"))
+# print(text[n:])
+
+# question 5:
+
+# a = 5
+# b = 10
+# print(f"before swap: a = {a}, b = {b}")
+# a = b
+# b = a
+# print(f"after swap: a = {a}, b = {b}")
+
+# # question 6:
+# number = int(5)
+# factorial = 1
+# for i in range(number):
+#     factorial *= i+1
+# print(factorial)
+
+# question 7:
+
 
